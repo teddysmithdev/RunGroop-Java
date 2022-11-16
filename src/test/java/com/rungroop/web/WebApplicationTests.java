b1eec0666c36868f459c7dc11ce0b5609c5f5e3b
teddysmithdev <teddysmithdev@gmail.com>
2022-11-16 07:50:12 -0500

    Intro + DB Setup

---
+++ src/test/java/com/rungroop/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rungroop.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
