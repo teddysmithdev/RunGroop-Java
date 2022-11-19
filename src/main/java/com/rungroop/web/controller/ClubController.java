fa2c6d0a465b3f79886a7497359fd3a8c85c9b00
teddysmithdev <teddysmithdev@gmail.com>
2022-11-19 07:56:44 -0500

    Controller + List View

---
+++ src/main/java/com/rungroop/web/controller/ClubController.java
@@ -0,0 +1,27 @@
+package com.rungroop.web.controller;
+
+import com.rungroop.web.dto.ClubDto;
+import com.rungroop.web.service.ClubService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class ClubController {
+    private ClubService clubService;
+
+    @Autowired
+    public ClubController(ClubService clubService) {
+        this.clubService = clubService;
+    }
+
+    @GetMapping("/clubs")
+    public String listClubs(Model model) {
+        List<ClubDto> clubs = clubService.findAllClubs();
+        model.addAttribute("clubs", clubs);
+        return "clubs-list";
+    }
+}
--- src/main/java/com/rungroop/web/service/impl/ClubServiceImpl.java
+++ src/main/java/com/rungroop/web/service/impl/ClubServiceImpl.java
@@ -5,10 +5,12 @@ import com.rungroop.web.models.Club;
 import com.rungroop.web.repository.ClubRepository;
 import com.rungroop.web.service.ClubService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class ClubServiceImpl implements ClubService {
     private ClubRepository clubRepository;
 
