80c42ad1a055f78163b3094adf65c14e68940732
teddysmithdev <teddysmithdev@gmail.com>
2022-11-29 08:22:34 -0500

    Detail

--- src/main/java/com/rungroop/web/controller/ClubController.java
+++ src/main/java/com/rungroop/web/controller/ClubController.java
@@ -31,6 +31,13 @@ public class ClubController {
         return "clubs-list";
     }
 
+    @GetMapping("/clubs/{clubId}")
+    public String clubDetail(@PathVariable("clubId") long clubId, Model model) {
+        ClubDto clubDto = clubService.findClubById(clubId);
+        model.addAttribute("club", clubDto);
+        return "clubs-detail";
+    }
+
     @GetMapping("/clubs/new")
     public String createClubForm(Model model) {
         Club club = new Club();
